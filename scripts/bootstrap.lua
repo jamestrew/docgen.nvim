03eff1e62d92828ee072ab40ff787c56b00d9ad3
James Trew <j.trew10@gmail.com>
2024-07-16 23:11:50 -0400

    create bootstrap script

---
+++ scripts/bootstrap.lua
@@ -0,0 +1,31 @@
+if vim.env.DOCGEN_PATH and not vim.uv.fs_stat(vim.env.DOCGEN_PATH) then
+  vim.env.DOCGEN_PATH = nil
+end
+
+local docgen_path = vim.env.DOCGEN_PATH or "./docgen"
+if not vim.env.DOCGEN_PATH and not vim.uv.fs_stat(docgen_path) then
+  vim.api.nvim_echo({
+    {
+      "Cloning docgen.nvim\n\n",
+      "DiagnosticInfo",
+    },
+  }, true, {})
+  local docgen_repo = "https://github.com/jamestrew/docgen.nvim.git"
+  local ok, out = pcall(vim.fn.system, {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    docgen_repo,
+    docgen_path,
+  })
+  if not ok or vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone docgen.nvim\n", "ErrorMsg" },
+      { vim.trim(out or ""), "WarningMsg" },
+      { "\nPress any key to exit...", "MoreMsg" },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(docgen_path)
